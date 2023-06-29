8037f58463997359c6dfe1f4ae35c8b7835cbb15
akash-soni <akash.200287@gmail.com>
2023-06-29 17:27:32 +0530

    Data ingestion complted

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from cdisease.logging import logger
+from cdisease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome")
+
+STAGE_NAME = "DATA_INGESTION_STAGE"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- template.py
+++ template.py
@@ -19,7 +19,7 @@ list_of_file = [
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init_.py",
     f"src/{project_name}/constants/__init__.py",
-    "config/config/yaml",
+    "config/config.yaml",
     "dvc.yaml",
     "templates/index.html",
     "params.yaml",
