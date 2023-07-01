8ced91454eae76b9652b7ad78bbdf5f513293b90
akash-soni <akash.200287@gmail.com>
2023-07-01 08:25:10 +0530

    training completed

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from cdisease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipel
 from cdisease.pipeline.stage_02_prepare_base_mode import (
     PrepareBaseModelTrainingPipeline,
 )
+from cdisease.pipeline.stage_03_training import ModelTrainingPipeline
+
 
 STAGE_NAME = "DATA_INGESTION_STAGE"
-
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -23,6 +24,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "MODEL_TRAINING_STAGE"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
