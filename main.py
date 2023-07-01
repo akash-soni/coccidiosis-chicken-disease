6fe0de4084f2b3db449e6ca2629257aeb8cf5f62
akash-soni <akash.200287@gmail.com>
2023-07-01 10:00:21 +0530

    model evaluation completed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from cdisease.pipeline.stage_02_prepare_base_mode import (
     PrepareBaseModelTrainingPipeline,
 )
 from cdisease.pipeline.stage_03_training import ModelTrainingPipeline
-
+from cdisease.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME = "DATA_INGESTION_STAGE"
 
@@ -36,6 +36,19 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     model_trainer = ModelTrainingPipeline()
     model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "MODEL_EVALUATION"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_evaluator = EvaluationPipeline()
+    model_evaluator.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
